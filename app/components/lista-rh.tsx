db810da39834f9afe033adc01f573b19935cd4cd
dwotV <daelcferreyra@gmail.com>
2025-05-04 11:19:06 -0600

    User status in table RH

--- app/components/lista-rh.tsx
+++ app/components/lista-rh.tsx
@@ -11,6 +11,7 @@ interface User {
   rol?: string;
   email?: string;
   telefono?: string;
+  estadoUsuario?: string;
 }
 
 const UserCard = ({ user }: { user: User }) => {
@@ -169,8 +170,11 @@ export default function ListRecursos() {
                 <th className="px-4 py-4 text-start text-[#495057] font-normal bg-white">
                   Correo
                 </th>
-                <th className="px-4 py-4 text-start text-[#495057] font-normal bg-white rounded-r-xl">
+                <th className="px-4 py-4 text-start text-[#495057] font-normal bg-white">
                   Teléfono
+                </th>
+                <th className="px-4 py-4 text-start text-[#495057] font-normal bg-white rounded-r-xl">
+                  Estado
                 </th>
               </tr>
             </thead>
@@ -204,8 +208,11 @@ export default function ListRecursos() {
                     <td className="px-4 py-4 bg-white">
                       {user.email || "N/A"}
                     </td>
-                    <td className="px-4 py-4 bg-white rounded-r-xl">
+                    <td className="px-4 py-4 bg-white">
                       {user.telefono || "N/A"}
+                    </td>
+                    <td className="px-4 py-4 bg-white rounded-r-xl">
+                      {user.estadoUsuario}
                     </td>
                   </tr>
                 ))
