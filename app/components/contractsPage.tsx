e5bed64a6b9ab69e2eee790ee00a9c3fd5983ff2
Esteban Leal <tebananas@hotmail.com>
2025-05-31 22:45:05 -0600

    contract management fix

--- app/components/contractsPage.tsx
+++ app/components/contractsPage.tsx
@@ -1,20 +1,20 @@
 import ShowCurrentContract from "./showCurrentContract";
-import ContractSendAndPreview from "./contractSendAndPreview";
-import ReviewContract from "./reviewContract";
 import { urbanist } from "./fonts";
+import ManageContracts from "./manageContracts";
 
 export default function ContractsPage({ uid }: { uid: string }) {
   return (
     <div className="mb-12">
-      <h2 className={`${urbanist.className} text-2xl text-[#212529] font-semibold`}>Contratos</h2>
+      <h2
+        className={`${urbanist.className} text-2xl text-[#212529] font-semibold`}
+      >
+        Contratos
+      </h2>
       <div className="flex flex-col md:flex-row gap-6">
         <ShowCurrentContract uid={uid} />
       </div>
-      <div>
-        <ContractSendAndPreview uid={uid} />
-      </div>
-      <div>
-        <ReviewContract uid={uid} />
+      <div className="mt-6">
+        <ManageContracts uid={uid} />
       </div>
     </div>
   );
