101a2b324fbe0ac7021cd11ce344180c85f63d22
dwotV <daelcferreyra@gmail.com>
2025-06-02 19:03:38 -0600

    UI Changes: Little changes in expediente UI

--- app/components/expedienteRH.tsx
+++ app/components/expedienteRH.tsx
@@ -41,9 +41,9 @@ export default function ExpedienteRH({ userId }: ExpedienteRHProps) {
   }, [userId]);
 
   return (
-    <div className="flex flex-col lg:flex-row gap-4 p-4 bg-gray-50 min-h-screen">
+    <div className="flex flex-col lg:flex-row gap-4 min-h-screen mb-6">
       {/* Lado izquierdo */}
-      <div className="lg:w-1/4 w-full bg-white border rounded-xl shadow p-4">
+      <div className="lg:w-1/4 w-full bg-white rounded-xl shadow p-4">
         <div className="flex items-center justify-between mb-4 space-x-3">
           <h2 className="text-lg font-semibold text-gray-800">Documentos</h2>
           <DownloadBatchFilesButton expedienteId={userId} />
@@ -53,7 +53,7 @@ export default function ExpedienteRH({ userId }: ExpedienteRHProps) {
             <li key={doc.id}>
               <button
                 onClick={() => setDocumentoId(doc.id)}
-                className={`w-full text-left px-4 py-2 rounded-md transition
+                className={`w-full text-left px-4 py-2 rounded-md transition cursor-pointer
                   ${
                     documentoId === doc.id
                       ? "bg-blue-100 text-blue-800 font-semibold"
@@ -68,7 +68,7 @@ export default function ExpedienteRH({ userId }: ExpedienteRHProps) {
       </div>
 
       {/* Lado derecho */}
-      <div className="flex-1 bg-white border rounded-xl shadow p-4">
+      <div className="flex-1 bg-white rounded-xl shadow p-4">
         <div className="flex justify-between items-center mb-4">
           <h3 className="text-xl font-semibold text-gray-800">
             {DOCUMENTOS.find((doc) => doc.id === documentoId)?.nombre}
