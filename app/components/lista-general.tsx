410b6e357b2ae180beead28283ccdfe7d8bb4447
dwotV <daelcferreyra@gmail.com>
2025-06-06 00:31:22 -0600

    UI Changes: New style to persons cards

--- app/components/lista-general.tsx
+++ app/components/lista-general.tsx
@@ -75,7 +75,7 @@ export default function ListUsers() {
       />
 
       {activo === "grid" ? (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 content-center">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 content-center">
           {sortedUsers.map((user) => (
             <UserCard key={user.id} user={user} />
           ))}
