5233876e71a99b82464a689d639456d596a7d636
Andrea Bahena <andreabahenavs@gmail.com>
2025-06-09 10:46:33 -0600

    Correct link

--- app/components/olvideContrasena.tsx
+++ app/components/olvideContrasena.tsx
@@ -189,7 +189,7 @@ export default function FormularioOlvide() {
               await sendPasswordResetEmail(auth, email, {
                 url: process.env.NODE_ENV === 'development'
                 ? "http://localhost:3000/olvidaste/link"
-                : "https://sistema-contrataciones-grupo-pissa-b8fo.vercel.app/olvidaste/link",
+                : "https://www.grupo-pissa.space/olvidaste/link",
                 handleCodeInApp: true
               });
               console.log("Correo de recuperación enviado");
