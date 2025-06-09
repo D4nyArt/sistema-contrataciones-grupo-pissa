1570d4c14233d9ba9ebc24bc09cff631ae9e8f9b
dwotV <daelcferreyra@gmail.com>
2025-06-08 23:25:07 -0600

    UI Changes: Scroll changes and skeleton was created in userinfo.tsx to improve the user experience

--- app/components/expedienteRH.tsx
+++ app/components/expedienteRH.tsx
@@ -44,27 +44,29 @@ export default function ExpedienteRH({ userId }: ExpedienteRHProps) {
     <div className="flex flex-col lg:flex-row gap-4 min-h-screen mb-6">
       {/* Lado izquierdo */}
       <div className="lg:w-1/4 w-full bg-white rounded-xl shadow p-4">
-        <div className="flex items-center justify-between mb-4 space-x-3">
+        <div className="flex items-center justify-between mb-1 space-x-3 border-b pb-4 border-gray-200">
           <h2 className="text-lg font-semibold text-gray-800">Documentos</h2>
           <DownloadBatchFilesButton expedienteId={userId} />
         </div>
-        <ul className="space-y-2">
-          {DOCUMENTOS.map((doc) => (
-            <li key={doc.id}>
-              <button
-                onClick={() => setDocumentoId(doc.id)}
-                className={`w-full text-left px-4 py-2 rounded-md transition cursor-pointer
-                  ${
-                    documentoId === doc.id
-                      ? "bg-blue-100 text-blue-800 font-semibold"
-                      : "hover:bg-gray-200 text-gray-700"
-                  }`}
-              >
-                {doc.nombre}
-              </button>
-            </li>
-          ))}
-        </ul>
+        <div className="overflow-y-scroll h-[calc(100vh-200px)]">
+          <ul className="space-y-2">
+            {DOCUMENTOS.map((doc) => (
+              <li key={doc.id}>
+                <button
+                  onClick={() => setDocumentoId(doc.id)}
+                  className={`w-full text-left px-4 py-2 rounded-md transition cursor-pointer
+                    ${
+                      documentoId === doc.id
+                        ? "bg-blue-100 text-blue-800 font-semibold"
+                        : "hover:bg-gray-200 text-gray-700"
+                    }`}
+                >
+                  {doc.nombre}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
 
       {/* Lado derecho */}
