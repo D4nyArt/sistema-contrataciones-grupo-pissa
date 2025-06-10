a47f5087ba668bf3a37212be6d500adb0403b799
dwotV <daelcferreyra@gmail.com>
2025-06-09 20:02:32 -0600

    UI Changes: Phone version of Inicio was created

--- app/components/tablaSeguidosPhone.tsx
+++ app/components/tablaSeguidosPhone.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import ProfilePicture from "./profile-picture";
+//import EtiquetaEstado from "./etiquetaEstado";
 import { ArrowUpRight } from "lucide-react";
 import { usePathname, useRouter } from "next/navigation";
 
