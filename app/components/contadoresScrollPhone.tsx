3e12876130e1153bee437157996b935a47ff3c4d
dwotV <daelcferreyra@gmail.com>
2025-06-10 16:41:51 -0600

    UI Chamges: Counters changes

--- app/components/contadoresScrollPhone.tsx
+++ app/components/contadoresScrollPhone.tsx
@@ -6,32 +6,32 @@ import CantCorporativo from "./cantidad-usr-corporativo";
 export default function ContadoresPhoneScroll () {
     return(
         <div className="overflow-x-auto mt-6">
-            <div className="flex flex-row space-x-6 animate-fade-in-up w-max">
-                <div className="bg-white rounded-xl p-6 shadow-md flex flex-row justify-center items-center gap-8">
-                    <div className="bg-[#42b883] rounded-full p-4 text-white">
-                    <UserPlus />
-                    </div>
-                    <div className="flex flex-col">
-                    <h2 className="text-[#495057]">Candidatos</h2>
-                    <CantCandidatos />
-                    </div>
-                </div>
-                <div className="bg-white rounded-xl p-6 shadow-md flex flex-row justify-center items-center gap-8">
-                    <div className="bg-[#d55672] rounded-full p-4 text-white">
-                    <FolderOpenDot />
-                    </div>
-                    <div className="flex flex-col">
-                    <h2 className="text-[#495057]">Proyecto</h2>
-                    <CantProyectos />
+            <div className="flex flex-row space-x-6 animate-fade-in-up">
+                <div className="bg-white rounded-xl p-6 shadow-md flex flex-col justify-center items-center gap-8">
+                    <h2 className="text-[#495057]">Total de candidatos</h2>
+                    <div className="flex flex-row justify-center items-center space-x-6">
+                        <CantCandidatos />
+                        <div className="bg-[#42b883] rounded-full p-4 text-white">
+                            <UserPlus />
+                        </div>
                     </div>
                 </div>
-                <div className="bg-white rounded-xl p-6 shadow-md flex flex-row justify-center items-center gap-8 col-start-4 row-start-1">
-                    <div className="bg-[#aec5eb] rounded-full p-4 text-white">
-                    <Building />
+                <div className="bg-white rounded-xl p-6 shadow-md flex flex-col justify-center items-center gap-8">
+                    <h2 className="text-[#495057]">Total en proyecto</h2>
+                    <div className="flex flex-row justify-center items-center space-x-6">
+                        <CantProyectos />
+                        <div className="bg-[#d55672] rounded-full p-4 text-white">
+                            <FolderOpenDot />
+                        </div>
                     </div>
-                    <div className="flex flex-col">
-                    <h2 className="text-[#495057]">Corporativo</h2>
-                    <CantCorporativo />
+                </div>
+                <div className="bg-white rounded-xl p-6 shadow-md flex flex-col justify-center items-center gap-8 col-start-4 row-start-1">
+                    <h2 className="text-[#495057]">Total en corporativo</h2>
+                    <div className="flex flex-row justify-center items-center space-x-6">
+                        <CantCorporativo />
+                        <div className="bg-[#aec5eb] rounded-full p-4 text-white">
+                            <Building />
+                        </div>
                     </div>
                 </div>
             </div>
--- app/components/recoverAccount.tsx
+++ app/components/recoverAccount.tsx
@@ -4,7 +4,6 @@
 
 import { useEffect, useState } from "react";
 import ProfilePicture from "./profile-picture";
-import { Table2 } from "lucide-react";
 import { handleBlock, handleUnblock } from "../components/block";
 import { addHistoryEntry } from "../api/history/history";
 import { getAuth } from "firebase/auth";
@@ -28,7 +27,6 @@ export default  function ListUsers() {
   const [error, setError] = useState("");
   const [sortOption, setSortOption] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
-  const [activo, setActivo] = useState<"grid" | "tabla">("tabla");
   const [_estadoUsuario, setStatus] = useState("");
   const [_attempt, setAttempt] = useState(0);
   const [_time, setTime] = useState("");
@@ -131,38 +129,6 @@ export default  function ListUsers() {
 
   return (
     <main className="flex-1 p-4">
-      <div className="mb-4 flex gap-6 text-black animate-fade-in-up">
-        <input
-          type="text"
-          placeholder="Buscar"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          className="p-2 border border-gray-400 rounded-lg w-full bg-white"
-        />
-        <select
-          value={sortOption}
-          onChange={(e) => setSortOption(e.target.value)}
-          className="cursor-pointer border p-1 pl-4 rounded-lg bg-[#2d4583] text-white hover:bg-[#08b177]"
-        >
-          <option value="">Ordenar por</option>
-          <option value="nombreAZ">Nombre A → Z</option>
-          <option value="nombreZA">Nombre Z → A</option>
-          <option value="apellidoAZ">Apellido A → Z</option>
-          <option value="apellidoZA">Apellido Z → A</option>
-        </select>
-        <div className="md:flex shadow-md bg-white rounded-l-lg rounded-r-lg hidden text-[#495057]">
-          <button
-            onClick={() => setActivo("tabla")}
-            className={`cursor-pointer rounded-lg p-1 pl-2 pr-2 transition-colors border ${
-              activo === "tabla"
-                ? "border-[#2d4583] text-[#2d4583]"
-                : "border-transparent hover:text-[#08b177]"
-            }`}
-          >
-            <Table2 />
-          </button>
-        </div>
-      </div>
 
       {users.length === 0 && !loading && (
         <div className="text-center p-4 bg-white rounded-lg shadow">
@@ -170,106 +136,103 @@ export default  function ListUsers() {
         </div>
       )}
 
-      {activo === "tabla" && (
-        <div>
-          <table className="table-auto w-full border-separate border-spacing-y-2 animate-fade-in-up">
-            <thead>
-              <tr className="shadow-xs rounded-xl">
-                <th className="px-4 py-4 text-start text-[#495057] font-normal bg-white rounded-l-xl">
-                  Nombre
-                </th>
-                <th className="px-4 py-4 text-start text-[#495057] font-normal bg-white">
-                  Rol
-                </th>
-                <th className="px-4 py-4 text-start text-[#495057] font-normal bg-white">
-                  Correo
-                </th>
-                <th className="px-4 py-4 text-center text-[#495057] font-normal bg-white rounded-r-xl">
-                  Recuperación
-                </th>
+      <div>
+        <table className="table-auto w-full border-separate border-spacing-y-2 animate-fade-in-up">
+          <thead>
+            <tr className="shadow-xs rounded-xl">
+              <th className="px-4 py-4 text-start text-[#495057] font-normal bg-white rounded-l-xl">
+                Nombre
+              </th>
+              <th className="px-4 py-4 text-start text-[#495057] font-normal bg-white">
+                Rol
+              </th>
+              <th className="px-4 py-4 text-start text-[#495057] font-normal bg-white">
+                Correo
+              </th>
+              <th className="px-4 py-4 text-center text-[#495057] font-normal bg-white rounded-r-xl">
+                Recuperación
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {sortedUsers.length === 0 ? (
+              <tr>
+                <td
+                  className="border-b border-gray-300 px-4 py-4 text-center bg-white rounded-xl"
+                  colSpan={6}
+                >
+                  No se encontraron candidatos en proceso de recuperación.
+                </td>
               </tr>
-            </thead>
-            <tbody>
-              {sortedUsers.length === 0 ? (
-                <tr>
-                  <td
-                    className="border-b border-gray-300 px-4 py-4 text-center bg-white rounded-xl"
-                    colSpan={6}
+            ) : (
+              sortedUsers.map((user) => (
+                  <tr
+                    key={user.id}
+                    className={`transition-all duration-300 ease-in-out ${
+                      removingUserId === user.id ? "opacity-0 -translate-y-2" : ""
+                    }`}
                   >
-                    No se encontraron candidatos en proceso de recuperación.
+
+                  <td className="font-semibold px-4 py-4 bg-white rounded-l-xl flex flex-row items-center gap-2">
+                    <ProfilePicture
+                      nombre={`${user.nombre || ""}`}
+                      width={"w-8"}
+                      height={"h-8"}
+                      textSize={"text-xl"}
+                    />
+                    {user.nombre || "N/A"} {user.apellidos || "N/A"}
+                  </td>
+                  <td className="px-4 py-4 bg-white">
+                    <div className="bg-blue-100 text-blue-800 rounded-lg text-center">
+                      {user.rol || "N/A"}
+                    </div>
+                  </td>
+                  <td className="px-4 py-4 bg-white">
+                    {user.email || "N/A"}
+                  </td>
+                  <td className="px-4 py-4 bg-white rounded-r-xl text-center">
+                    <div className="flex justify-center gap-4">
+                      <button
+                        className="px-6 py-2 rounded bg-green-500 text-white transition-all duration-200 hover:bg-green-600 hover:shadow-lg hover:scale-105 focus:outline-none"
+                        title="Aprobar"
+                        onClick={async () => {
+                          setRemovingUserId(user.id);
+                          setTimeout(async () => {
+                            handleUnblock(user.id, user.estadoUsuario || "", setStatus, setAttempt, setTime);
+                            setUsers((prev) => prev.filter((u) => u.id !== user.id));
+                            setRemovingUserId(null);
+                            await addHistoryEntry(user.id, 'contrasenas', new Date().toISOString(), rhID, 'Recuperación aprobada');
+                            await sendEmailNotification(user.id, 'Su recuperación de contraseña fue aprobada', 'Ahora puede acceder a su cuenta con sus nuevas credenciales');
+                          }, 300); 
+
+                        }}
+                      >
+                        Aprobado
+                      </button>
+                      <button
+                        className="px-6 py-2 rounded bg-red-500 text-white transition-all duration-200 hover:bg-red-600 hover:shadow-lg hover:scale-105 focus:outline-none"
+                        title="Denegar"
+                        onClick={async () => {
+                          setRemovingUserId(user.id);
+                          setTimeout(async () => {
+                            handleBlock(user.id, setStatus, user.estadoUsuario);
+                            setUsers((prev) => prev.filter((u) => u.id !== user.id));
+                            setRemovingUserId(null);
+                            await addHistoryEntry(user.id, 'contrasenas', new Date().toISOString(), rhID, 'Recuperación denegada');
+                            await sendEmailNotification(user.id, 'Su recuperación de contraseña fue denegada', 'No se aprobó su cambio de contraseña, por favor, contacte al administrador.');
+                          }, 300);
+                        }}
+                      >
+                        Denegado
+                      </button>
+                    </div>
                   </td>
                 </tr>
-              ) : (
-                sortedUsers.map((user) => (
-                    <tr
-                      key={user.id}
-                      className={`transition-all duration-300 ease-in-out ${
-                        removingUserId === user.id ? "opacity-0 -translate-y-2" : ""
-                      }`}
-                    >
-
-
-                    <td className="font-semibold px-4 py-4 bg-white rounded-l-xl flex flex-row items-center gap-2">
-                      <ProfilePicture
-                        nombre={`${user.nombre || ""}`}
-                        width={"w-8"}
-                        height={"h-8"}
-                        textSize={"text-xl"}
-                      />
-                      {user.nombre || "N/A"} {user.apellidos || "N/A"}
-                    </td>
-                    <td className="px-4 py-4 bg-white">
-                      <div className="bg-blue-100 text-blue-800 rounded-lg text-center">
-                        {user.rol || "N/A"}
-                      </div>
-                    </td>
-                    <td className="px-4 py-4 bg-white">
-                      {user.email || "N/A"}
-                    </td>
-                    <td className="px-4 py-4 bg-white rounded-r-xl text-center">
-                      <div className="flex justify-center gap-4">
-                        <button
-                          className="px-6 py-2 rounded bg-green-500 text-white transition-all duration-200 hover:bg-green-600 hover:shadow-lg hover:scale-105 focus:outline-none"
-                          title="Aprobar"
-                          onClick={async () => {
-                            setRemovingUserId(user.id);
-                            setTimeout(async () => {
-                              handleUnblock(user.id, user.estadoUsuario || "", setStatus, setAttempt, setTime);
-                              setUsers((prev) => prev.filter((u) => u.id !== user.id));
-                              setRemovingUserId(null);
-                              await addHistoryEntry(user.id, 'contrasenas', new Date().toISOString(), rhID, 'Recuperación aprobada');
-                              await sendEmailNotification(user.id, 'Su recuperación de contraseña fue aprobada', 'Ahora puede acceder a su cuenta con sus nuevas credenciales');
-                            }, 300); 
-
-                          }}
-                        >
-                          Aprobado
-                        </button>
-                        <button
-                          className="px-6 py-2 rounded bg-red-500 text-white transition-all duration-200 hover:bg-red-600 hover:shadow-lg hover:scale-105 focus:outline-none"
-                          title="Denegar"
-                          onClick={async () => {
-                            setRemovingUserId(user.id);
-                            setTimeout(async () => {
-                              handleBlock(user.id, setStatus, user.estadoUsuario);
-                              setUsers((prev) => prev.filter((u) => u.id !== user.id));
-                              setRemovingUserId(null);
-                              await addHistoryEntry(user.id, 'contrasenas', new Date().toISOString(), rhID, 'Recuperación denegada');
-                              await sendEmailNotification(user.id, 'Su recuperación de contraseña fue denegada', 'No se aprobó su cambio de contraseña, por favor, contacte al administrador.');
-                            }, 300);
-                          }}
-                                                  >
-                          Denegado
-                        </button>
-                      </div>
-                    </td>
-                  </tr>
-                ))
-              )}
-            </tbody>
-          </table>
-        </div>
-      )}
+              ))
+            )}
+          </tbody>
+        </table>
+      </div>
     </main>
   );
 }
