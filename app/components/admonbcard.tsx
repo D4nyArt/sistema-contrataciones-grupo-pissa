5704fe21007a8cf998fda7c2edc158b77dc6acf3
Daniel Arteaga <dandavarteaga@gmail.com>
2025-06-13 16:34:41 -0600

    minor change

--- app/components/admonbcard.tsx
+++ app/components/admonbcard.tsx
@@ -36,7 +36,7 @@ export default function AdminOnboardingPage() {
       </button>
       <div className="parent grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 grid-rows-2 gap-x-4 gap-y-10 content-center">
         {Object.entries(onbCards).map(([key, card]) => (
-          <OnboardingCard key={key} nombre={card.nombre} url={card.url} type = {card.type} accepted = {card.accepted} />
+          <OnboardingCard key={key} nombre={card.nombre} url={card.url} type = {card.type} />
         ))}
       </div>
       <PopUp show = {showConfirm} onClose={() => setShowConfirm(false)}>
