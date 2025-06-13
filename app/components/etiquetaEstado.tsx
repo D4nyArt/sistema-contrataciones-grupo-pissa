12b9a640664535cb92ece82e003fa6a70d926235
dwotV <daelcferreyra@gmail.com>
2025-06-13 10:09:42 -0600

    UI Changes: New status labels

--- app/components/etiquetaEstado.tsx
+++ app/components/etiquetaEstado.tsx
@@ -1,4 +1,5 @@
-import { CircleCheck, Lock, UserMinus, Clock, Undo } from "lucide-react";
+// components/EtiquetaEstado.tsx
+import { CircleCheck, Lock, UserMinus, Clock, Undo, RotateCcwIcon } from "lucide-react";
 
 interface EtiquetaEstadoProps {
   status: string;
@@ -41,6 +42,12 @@ const statusMap = {
     text: "text-gray-800",
     label: "Previo",
   },
+  cambioContrasena: {
+    icon: <RotateCcwIcon className="size-4 text-gray-800" />,
+    bg: "bg-gray-200",
+    text: "text-gray-800",
+    label: "Solicitud"
+  }
 } as const;
 
 export default function EtiquetaEstado({ status }: EtiquetaEstadoProps) {
